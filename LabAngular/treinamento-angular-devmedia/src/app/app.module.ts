5ef96721fd4ecf028c26ebbde5449df1cc553b75
Fernando Teixeira Silva <odnanref_007@hotmail.com>
2018-12-14 21:31:36 -0200

    Checkin

--- LabAngular/treinamento-angular-devmedia/src/app/app.module.ts
+++ LabAngular/treinamento-angular-devmedia/src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
 import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
-import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
+import { MenuLateralComponent } from './barra-navegacao/menu-lateral/menu-lateral.component';
 import { ResumoComponent } from './resumo/resumo.component';
 import { ConsultaComponent } from './consulta/consulta.component';
 import { ConsultaPorEspecialidadeComponent } from './consulta/consulta-por-especialidade/consulta-por-especialidade.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ResumoService } from './resumo/resumo.service';
+import { ConsultaService } from './consulta/consulta.service';
+import { FaturamentoComponent } from './faturamento/faturamento.component';
+import { FaturamentoService } from './faturamento/faturamento.service';
+import { TesteModule } from './teste/teste.module';
+import { OlaMundoComponent } from './ola-mundo/ola-mundo.component';
 
-let routes = [
-  { path: 'resumo', component: ResumoComponent },
-  { path: 'consultas', component: ConsultaComponent },
-  { path: "**", redirectTo: '/resumo' }
-];
 
 @NgModule({
   declarations: [
@@ -24,13 +26,16 @@ let routes = [
     MenuLateralComponent,
     ResumoComponent,
     ConsultaComponent,
-    ConsultaPorEspecialidadeComponent
+    ConsultaPorEspecialidadeComponent,
+    FaturamentoComponent,
+    OlaMundoComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ResumoService, ConsultaService, FaturamentoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
